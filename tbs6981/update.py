f6be518bbf914c5b850d303c756f35867864cc36
chwiede <chwiede@gmail.com>
2013-10-01 18:28:26 +0200

    workdir changed to tmp

--- tbs6981/update.py
+++ tbs6981/update.py
@@ -9,8 +9,8 @@ from os.path import expanduser
 
 # settings
 home = expanduser("~")
-workdir = os.path.join(home, 'sources/tbsdtv/')
-weburl = 'http://www.tbsdtv.com/download/document/common/'
+workdir = '/tmp/tbsdtv/'
+weburl = 'http://www.tbsdtv.com/download/'
 
 # load site
 source = urllib.urlopen(weburl).read()
@@ -20,12 +20,12 @@ pattern = 'tbs-linux-drivers_v[0-9]+.zip'
 foundLinks = re.findall(pattern, source)
 foundLinks.sort()
 usefile = foundLinks[-1] 
-webfile = weburl + usefile
+webfile = weburl + 'document/common/' + usefile
 locfile = os.path.realpath(workdir + usefile)
 
 # downloaded already?
 if(not os.path.isfile(locfile)):
-    print 'downloading file "%s".' % usefile
+    print 'downloading file "%s".' % webfile
     os.system('mkdir -p ' + workdir)
     urllib.urlretrieve (webfile, locfile)
 else:
